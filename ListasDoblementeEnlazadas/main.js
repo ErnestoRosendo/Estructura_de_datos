a9a13df2a9aa7d7889cc179bca35c38e08735baf
Ernesto Rosendo <erosendo@ucol.mx>
2023-10-12 13:58:10 -0600

    terminando programa con listas doblemente enlazadas

--- ListasDoblementeEnlazadas/main.js
+++ ListasDoblementeEnlazadas/main.js
@@ -29,12 +29,15 @@ class Nodo{
         return this._recListar(this.primero);
     }
 
-    buscar(dato){
-        let aux = this.primero
-        while (aux.dato !== dato) {
-            aux = aux.siguiente
+    buscar(dato) {
+      let aux = this.primero;
+      while (aux !== null) {
+        if (aux.dato === dato) {
+          return aux;
         }
-        return aux
+        aux = aux.siguiente;
+      }
+      return null; 
     }
 
     agregarInicio(dato){
@@ -44,8 +47,12 @@ class Nodo{
     }
 
     agregarAntes(dato, nuevo){
-
-        let aux = this.primero
+      let aux = this.primero
+        if (dato === aux.dato) {
+          nuevo.siguiente = aux
+          this.primero.anterior = nuevo
+          this.primero = nuevo
+        } else {
         while (aux.dato != dato) {
             aux = aux.siguiente
         }
@@ -53,44 +60,77 @@ class Nodo{
         nuevo.anterior = aux.anterior
         aux.anterior.siguiente = nuevo
         aux.anterior = nuevo
+        }
     }
 
     agregarDespues(dato, nuevo){
         let aux = this.primero
-        while (aux.dato != dato) {
-            aux = aux.siguiente
+        while (aux.siguiente != null) {
+          if (aux.dato == dato) {
+
+            nuevo.siguiente = aux.siguiente
+            nuevo.anterior = aux
+            aux.siguiente.anterior = nuevo
+            aux.siguiente = nuevo
+            
+          }
+          aux = aux.siguiente
         }
-        nuevo.siguiente = aux.siguiente
-        nuevo.anterior = aux
-        aux.siguiente.anterior = nuevo
-        aux.siguiente = nuevo
+        if (aux.dato == dato) {
+
+          nuevo.anterior = aux
+          aux.siguiente = nuevo
+          
+        }
         
     }
 
     extraer(dato){
-        aux = this.primero
-        while (aux.siguiente != null) {
-            if (aux.dato == dato) {
-                
-            }
-            aux = aux.siguiente
+      let aux = this.primero;
+      while (aux !== null) {
+        if (aux.dato === dato) {
+          if (aux.anterior == null) {
+            aux.siguiente.anterior = null;
+            this.primero = aux.siguiente
+            return aux
+          } else if (aux.siguiente == null) {
+            aux.anterior.siguiente = null
+            return aux
+          } else {
+            aux.anterior.siguiente = aux.siguiente;
+            aux.siguiente.anterior = aux.anterior;
+            return aux
+          }
         }
+        aux = aux.siguiente;
+      }
+      
+      return null; 
+      
     }
 
     _recListar(nodo){
       if (nodo.siguiente==null)
         return nodo.dato;
       else
-        return nodo.dato + ' ' + this._recListar(nodo.siguiente);
+      return nodo.dato + ' ' + this._recListar(nodo.siguiente);
     }
 
 
+    _recListarInverso(nodo){
+      if (nodo.siguiente==null)
+        return nodo.dato;
+      else
+      return this._recListarInverso(nodo.siguiente) + ' ' + nodo.dato;
+    }
+    
 
-  listarInverso(nodo){
-    if (nodo.siguiente==null)
-      return nodo.dato;
-    else
-      return nodo.dato + ' ' + this._recListar(nodo.siguiente);
+
+  listarInverso(){
+    if (this.primero==null)
+        return "";
+      else
+        return this._recListarInverso(this.primero);
   }
 }
 
@@ -108,7 +148,7 @@ class Nodo{
   grupo.agregar(nuevo);
   nuevo=new Nodo("T");
   grupo.agregar(nuevo);  
-  nuevo=new Nodo("M");
+  nuevo=new Nodo("U");
   grupo.agregar(nuevo);
   nuevo=new Nodo("A");
   grupo.agregarInicio(nuevo);
@@ -116,4 +156,6 @@ class Nodo{
   grupo.agregarAntes("F", nuevo);       
   nuevo =new Nodo("C");
   grupo.agregarDespues("B", nuevo);
+  nuevo =new Nodo("Z");
+  grupo.agregarDespues("U", nuevo);
   console.log(grupo.listar());
